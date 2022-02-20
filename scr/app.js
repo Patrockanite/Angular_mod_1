69fb593331ac21b050a5e8ed647be63dd2e80161
Patrockanite <patrickannoot@orange.fr>
2022-02-20 10:47:47 +0100

    solution en ligne

--- scr/app.js
+++ scr/app.js
@@ -3,14 +3,31 @@
     angular.module('LunchCheck',[]).controller('LC_Controller',LC_Controller);
     LC_Controller.$inject=['$scope'];
 
-    function LC_Controller($scope, $filter, $injector){
+    function LC_Controller($scope){ 
         $scope.petits_plats = "";
+        $scope.diet_message ="" ;
 
-        $scope.upper = function(){
-            var upCase = $filter('uppercase');
-            $scope.name = upCase($scope.name);
-            
+        $scope.diet_check = function(){
+            var NbFood =  $scope.countFood();
+            console.log(NbFood);
+            if(NbFood>0 && NbFood<=3){
+                $scope.diet_message = "Enjoy!";
+            }
+            if(NbFood>3){
+                $scope.diet_message = "Too much!";
+            }
+           
         };
+        $scope.countFood = function(){
+            var Tableau =[];
+            if($scope.petits_plats.split(",")[0]===""){
+                return 0;
+            }
+            else{                
+            return $scope.petits_plats.split(",").length;
+            }
+        };
+       
         
     }
    
